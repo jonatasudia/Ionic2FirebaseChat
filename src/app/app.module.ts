9d8d821a6668f4e053271700853aae2a1e26a965
Jonatas Barbosa <jonatasudia@gmail.com>
2018-04-11 16:19:50 -0300

    Versao com tela de cadastro de usuario, configuracao Firebase e botao sign na pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import {AngularFireModule, FirebaseAppConfig} from 'angularfire2';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -6,25 +8,41 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SignupPage } from '../pages/signup/signup';
+import { UserService } from './../providers/user/user.service';
+
+
+const firebaseAppConfig: FirebaseAppConfig ={
+  apiKey: "AIzaSyDr8wJPVO6sY7agu9r6ugfcsd0Jxt2b-po",
+  authDomain: "ionic2-firebase-chat-8cde7.firebaseapp.com",
+  databaseURL: "https://ionic2-firebase-chat-8cde7.firebaseio.com",
+  storageBucket: "ionic2-firebase-chat-8cde7.appspot.com",
+  messagingSenderId: "259957710048"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAppConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SignupPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserService
   ]
 })
 export class AppModule {}
---
+++ src/providers/user/user.service.ts
@@ -0,0 +1,22 @@
+import { AngularFireDatabase } from "angularfire2/database"; 
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from './../../models/user.model';
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    public db: AngularFireDatabase,
+    public http: HttpClient) {
+    
+  }
+
+  create(user: User): Promise<void>{
+    return this.db.object(`/users`)
+    .set(user);
+  }
+ 
+
+}
